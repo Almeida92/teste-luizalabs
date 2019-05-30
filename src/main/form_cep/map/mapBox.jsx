280d2e4554ca08b6f2a08657d683b789d50e18bf
Felipe Almeida <felipe.almeida@jsl.com.br>
2019-05-30 11:31:49 -0300

    api de geolocalização

--- src/main/form_cep/map/mapBox.jsx
+++ src/main/form_cep/map/mapBox.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
+const URL = 'https://api.opencagedata.com/geocode/v1/json?q=';
+const KEY = '439511cb7911414d94c0ecf592eebd0b';
 
 class MapBox extends Component {
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
+        axios.get(`${URL}${this.props.endereco.localidade.replace(' ', '%20')}&key=${KEY}`)
+            .then(resp => {
+                let coord = resp.data.results[0].geometry;
+            })
     }
 
     render() {
